5b88fa6d847bcc49f460bc12a7949738d8e72b51
vnshas <163927528+vnshas@users.noreply.github.com>
2024-05-07 16:48:28 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+  1. Selecione o elemento de id "ingredients" e o atribua a uma constante.  
+  Mostre somente o elemento selecionado no console.
+*/
+
+/*
+  2. Selecione o elemento <h1> e o atribua a uma constante.
+   Mostre somente o elemento selecionado no console.
+*/
+
+/*
+  3. Selecione o elemento com classe "recipe" e o atribua a uma constante.
+   Mostre somente elemento selecionado no console.
+*/
+
+/*
+  Desafio 1: Selecione todos os elementos <h2> da página e altere a cor do texto de
+   todos para azul.
+*/
+
+/* 
+  Desafio 2: Selecione todos os elementos <input> da página, e altere a cor da borda de
+   todos para vermelho.
+*/
